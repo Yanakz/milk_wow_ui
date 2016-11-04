719553a1627d79144f933b8fbe53cfe603850c55
YanaKz <hrliang1988@gmail.com>
2016-11-04 13:20:15 +0800

    Add files via upload

---
+++ MilkUI_lib/MilkUI_enhance.lua
@@ -0,0 +1,85 @@
+﻿
+--聊天框显示拾取图标
+local function AddLootIcons(self, event, message, ...)
+   local function Icon(link)
+      local texture = GetItemIcon(link)
+      return "\124T" .. texture .. ":" .. 12 .. "\124t" .. link
+   end
+   message = message:gsub("(\124c%x+\124Hitem:.-\124h\124r)", Icon)
+   return false, message, ...
+end
+
+ChatFrame_AddMessageEventFilter("CHAT_MSG_LOOT", AddLootIcons)
+
+
+
+--副本任务栏自动收起
+local autocollapse = CreateFrame("Frame")
+autocollapse:RegisterEvent("ZONE_CHANGED_NEW_AREA")
+autocollapse:RegisterEvent("PLAYER_ENTERING_WORLD")
+autocollapse:SetScript("OnEvent", function(self)
+if IsInInstance() then
+ObjectiveTrackerFrame.userCollapsed = true
+ObjectiveTracker_Collapse()
+else
+ObjectiveTrackerFrame.userCollapsed = nil
+ObjectiveTracker_Expand()
+end
+end)
+
+-- --自动贪婪绿色
+-- if(auto_greed==true)  then
+--    local agog = CreateFrame("Frame", nil, UIParent)
+--    agog:RegisterEvent("START_LOOT_ROLL")
+--    agog:SetScript("OnEvent", function(_, _, id)
+--    if not id then return end
+--    -- local _, _, _, _, _, canneed = GetLootRollItemInfo(id) --http://wowwiki.wikia.com/wiki/API_GetLootRollItemInfo 
+--    -- if canneed == 1 then RollOnLoot(id, 1 or 2) end
+--    local _, _, _, quality, bop, _, _, canDE = GetLootRollItemInfo(id)
+--    if quality == 2 then RollOnLoot(id, 2 or 3) end
+--    end)
+-- end
+
+-- --找出点小地图的傻逼
+-- Minimap:SetScript("OnMouseUp", function(self, btn)
+--    if btn == "RightButton" then
+--       ToggleDropDownMenu(1, nil, MiniMapTrackingDropDown, "MiniMapTracking", 0, -5)
+--       PlaySound("igMainMenuOptionCheckBoxOn")
+--    else
+--       Minimap_OnClick(self)
+--    end
+-- end)
+
+
+-- --skada内嵌
+
+-- ChatFrame1Tab:HookScript('OnClick', function(self, button, down)
+-- if (button == "LeftButton") then
+-- for i,win in ipairs(Skada:GetWindows()) do
+-- win:Hide();
+-- end;
+-- end;
+-- end);
+-- ChatFrame2Tab:HookScript('OnClick', function(self, button, down)
+-- if (button == "LeftButton") then
+-- for i,win in ipairs(Skada:GetWindows()) do
+-- win:Hide();
+-- end;
+-- end;
+-- end);
+-- ChatFrame3Tab:HookScript('OnClick', function(self, button, down)
+-- if (button == "LeftButton") then
+-- for i,win in ipairs(Skada:GetWindows()) do
+-- win:Hide();
+-- end;
+-- end;
+-- end);
+-- ChatFrame4Tab:HookScript('OnClick', function(self, button, down)
+-- if (button == "LeftButton") then
+-- for i,win in ipairs(Skada:GetWindows()) do
+-- win:Show();
+-- end;
+-- end;
+-- end);
+
+
---
+++ MilkUI_lib/MilkUI_tooltipenhancement.lua
@@ -0,0 +1,168 @@
+﻿--mktip
+--~ LoadAddOn("Blizzard_InspectUI")
+local PRIMARY_TALENT = SPECIALIZATION.." : "
+local SECONDARY_TALENT = TALENT.." : "
+local mksetting = {
+	showtalent = true,
+	Fade = true
+}
+local myspec
+
+
+--------------------------------------------------------------
+local function GetHexColor(color)
+  return ("%.2x%.2x%.2x"):format(color.r*255, color.g*255, color.b*255)
+end
+
+local classColors, reactionColors = {}, {}
+
+for class, color in pairs(RAID_CLASS_COLORS) do
+  classColors[class] = GetHexColor(RAID_CLASS_COLORS[class])
+end
+
+for i = 1, #FACTION_BAR_COLORS do
+  reactionColors[i] = GetHexColor(FACTION_BAR_COLORS[i])
+end
+
+
+
+
+--------------------------------------------------------------
+
+local function GetTarget(unit)
+  if UnitIsUnit(unit, "player") then
+    return ("|cffff0000%s|r"):format("<你>")
+  elseif UnitIsPlayer(unit, "player")then
+    return ("|cff%s%s|r"):format(classColors[select(2, UnitClass(unit))], UnitName(unit))
+  elseif UnitReaction(unit, "player") then
+    return ("|cff%s%s|r"):format(reactionColors[UnitReaction(unit, "player")], UnitName(unit))
+  else
+    return ("|cffffffff%s|r"):format(UnitName(unit))
+  end
+end
+
+--------------------------------------------------------------
+
+local function Gettalent(unit)
+    if (not unit) then return end 
+		
+    if UnitIsUnit(unit,"player") then
+	      Specid = GetSpecializationInfo(GetSpecialization())
+		elseif UnitIsPlayer(unit,"player") then
+		    
+		    Specid = GetInspectSpecialization(unit)
+				-- DEFAULT_CHAT_FRAME:AddMessage("this guy is "..Specid.." "..unit)
+		else
+		    return
+		end
+		return Specid
+end
+
+--------------------------------------------------------------
+
+-- locs
+
+
+local GameTooltip = GameTooltip
+GameTooltip:HookScript("OnTooltipSetUnit", function(self)
+	local _, unit = self:GetUnit()
+	if (not unit) then return end
+
+
+	-- local spName = Getspec(unit)
+	-- if (spName) then
+	--     text = "|cffffffff" .. spName
+	-- 		GameTooltip:AddLine(text)
+	-- end
+  -- NotifyInspect(unit) 
+  
+	-- if spec and mksetting.showtalent then
+	-- 		if tonumber(spec) then	--spec已读取到
+	-- 				if tonumber(spec)==0 then		--无专精
+	-- 						spec = "|cffffff00"..PRIMARY_TALENT.."|r".."|cff00ff00["..NONE.."]|r"
+	-- 			  else							--有专精
+	-- 						local _, specName, _, icon = GetSpecializationInfoByID(tonumber(spec))
+	-- 						if icon then 			--是否使用icon
+	-- 								icon = "|T"..icon..":12:12:0:0:10:10:0:10:0:10|t "
+	-- 						else
+	-- 								icon = ""
+	-- 						end
+	-- 				    spec = "|cffffff00"..PRIMARY_TALENT.."|r"..icon.."|cff00ff00["..specName.."]|r"
+	-- 			  end
+	-- 		else					--spec未读取到
+	-- 				spec = "|cffffff00"..PRIMARY_TALENT.."|r".."|cff00ff00"..spec.."|r"
+	-- 		end
+	-- 	  self:AddLine(spec)
+			
+  -- end
+
+	if (UnitIsUnit("player", unit .. "target")) then
+		self:AddDoubleLine("|cffff6666目标|r",GetTarget(unit.."target") or "Unknown")
+	elseif (UnitExists(unit .. "target")) then
+		self:AddDoubleLine("|cffff6666目标|r",GetTarget(unit.."target") or "Unknown")
+	end
+
+
+	local unitGuild = GetGuildInfo(unit)
+    local text = GameTooltipTextLeft2:GetText()
+    if unitGuild and text and text:find("^"..unitGuild) then
+      GameTooltipTextLeft2:SetText("<"..text..">")
+      GameTooltipTextLeft2:SetTextColor(255/255, 20/255, 200/255)
+    end
+
+
+end)
+----------------------------------------------------------------
+	GameTooltip:HookScript("OnTooltipCleared", function(self)
+		GameTooltip_ClearMoney(self)
+		GameTooltip_ClearStatusBars(self)
+		-- OnGameTooltipHide()
+	end)
+
+--------------------------------------------------------------
+
+
+hooksecurefunc(GameTooltip, "FadeOut", function(self)
+	if (not mksetting.Fade) then
+		GameTooltip:Hide();
+	end
+end)
+
+
+--------------------------------------------------------------
+function GameTooltip_UnitColor(unit)
+	local r, g, b
+	local reaction = UnitReaction(unit, "player")
+	if reaction then
+		r = FACTION_BAR_COLORS[reaction].r
+		g = FACTION_BAR_COLORS[reaction].g
+		b = FACTION_BAR_COLORS[reaction].b
+	else
+		r = 1.0
+		g = 1.0
+		b = 1.0
+	end
+	if UnitIsPlayer(unit) then
+		local class = select(2, UnitClass(unit))
+		r = RAID_CLASS_COLORS[class].r
+		g = RAID_CLASS_COLORS[class].g
+		b = RAID_CLASS_COLORS[class].b
+	end
+	return r, g, b
+end
+
+--------------------------------------------------------------
+
+-- -- local myf = CreateFrame("Frame")
+-- GameTooltip:SetScript('OnEvent', function(self,event,...)
+--     if (event == 'INSPECT_READY') then
+-- 				local _, unit = self:GetUnit()
+-- 				spec = Gettalent(unit)
+-- 				-- DEFAULT_CHAT_FRAME:AddMessage("123")   
+-- 		end
+-- end)
+-- GameTooltip:RegisterEvent("INSPECT_READY")
+-- NotifyInspect(0)
+
+
+
